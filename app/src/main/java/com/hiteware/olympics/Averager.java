71af095d1224bf13b047e8c76112dcf9e980a716
Jason Hite <hite77@gmail.com>
2016-05-24 22:34:12 -0400

    Update Average To return channel average, and also average averages for unknown stations

--- app/src/main/java/com/hiteware/olympics/Averager.java
+++ app/src/main/java/com/hiteware/olympics/Averager.java
@@ -1,10 +1,15 @@
 package com.hiteware.olympics;
 
+import java.util.ArrayList;
+
 /**
  * Created on 5/22/16.
  */
 public class Averager {
-    public float calculate(String time, String capacity) {
+    ArrayList<Float>  averages = new ArrayList<>();
+    ArrayList<String> channels = new ArrayList<>();
+
+    public float calculate(String time, String capacity, String Channel) {
         int firstColon = time.indexOf(":");
         int secondColon = time.indexOf(":",firstColon+1);
         float hrs = Float.valueOf(time.substring(0,firstColon));
@@ -19,6 +24,30 @@ public class Averager {
         else {
             gb =  Float.valueOf(capacity.replace("GB",""));
         }
-        return gb/timeFloat;
+        float average = gb/timeFloat;
+        int pos = channels.indexOf(Channel);
+        if (pos != -1) {
+            averages.set(pos, (float) ((average+averages.get(pos)) /2.0));
+        }
+        else {
+            averages.add(average);
+            channels.add(Channel);
+        }
+        return average;
+    }
+
+    public float retrieve(String Channel) {
+        int pos = channels.indexOf(Channel);
+        if (pos != -1) {
+            return averages.get(pos);
+        }
+
+        Float sum = (float) 0.0;
+        Float count = (float) 0.0;
+        for (Float average : averages) {
+            sum += average;
+            count += (float) 1.0;
+        }
+        return sum / count;
     }
 }
--- app/src/test/java/com/hiteware/olympics/averagePerHourTest.java
+++ app/src/test/java/com/hiteware/olympics/averagePerHourTest.java
@@ -13,28 +13,28 @@ public class averagePerHourTest {
     @Test
     public void canAverageGBPerHourGivenGB() {
         Averager averager = new Averager();
-        float average = averager.calculate("1:00:00", "1.00 GB");
+        float average = averager.calculate("1:00:00", "1.00 GB", "FMHD");
         assertThat(average, equalTo((float) 1.0));
     }
 
     @Test
     public void canAverageMultipleHourGivenGB() {
         Averager averager = new Averager();
-        float average = averager.calculate("3:00:00", "2.5 GB");
+        float average = averager.calculate("3:00:00", "2.5 GB", "RVLTHD");
         assertThat(average, equalTo((float) (2.5/3.0)));
     }
 
     @Test
     public void canAverageMultipleMinutesGivenGB() {
         Averager averager = new Averager();
-        float average = averager.calculate("2:15:00", "3.4 GB");
+        float average = averager.calculate("2:15:00", "3.4 GB", "MTVLIVE");
         assertThat(average, equalTo((float) (3.4/(2.0+15.0/60.0))));
     }
 
     @Test
     public void canAverageMultipleSecondsGivenGB() {
         Averager averager = new Averager();
-        float average = averager.calculate("0:23:42", "0.98 GB");
+        float average = averager.calculate("0:23:42", "0.98 GB", "TCMHD");
         float time = (float) (23.0/60.0+42.0/(60.0*60.0));
         assertThat(average, equalTo((float) (0.98/time)));
     }
@@ -42,14 +42,14 @@ public class averagePerHourTest {
     @Test
     public void canAverageGBPerHourGivenMB() {
         Averager averager = new Averager();
-        float average = averager.calculate("1:00:00", "728 MB");
+        float average = averager.calculate("1:00:00", "728 MB", "LMNHD");
         assertThat(average, equalTo((float) (728.0 / 1024.0)));
     }
 
     @Test
     public void canAverageMultipleHourGivenMB() {
         Averager averager = new Averager();
-        float average = averager.calculate("2:00:00", "500 MB");
+        float average = averager.calculate("2:00:00", "500 MB", "ESPNHD");
         assertThat(average, equalTo((float) ((500.0/ 1024.0)/ 2.0)));
     }
 
@@ -57,7 +57,7 @@ public class averagePerHourTest {
     @Test
     public void canAverageMultipleMinutesGivenMB() {
         Averager averager = new Averager();
-        float average = averager.calculate("2:53:00", "333 MB");
+        float average = averager.calculate("2:53:00", "333 MB", "SUNDHD");
         float fb = (float) (333.0/1024.0);
         float time = (float) (2.0 + 53.0/60.0);
         assertThat(average, equalTo(fb/ time));
@@ -66,15 +66,35 @@ public class averagePerHourTest {
     @Test
     public void canAverageMultipleSecondsGivenMB() {
         Averager averager = new Averager();
-        float average = averager.calculate("2:53:26", "444 MB");
+        float average = averager.calculate("2:53:26", "444 MB", "IFCHD");
         float fb = (float) (444.0/1024.0);
         float time = (float) (2.0 + 53.0/60.0+ 26/(60.0*60.0));
         assertThat(average, equalTo(fb/ time));
     }
 
+    @Test
+    public void canAverageAndRecall() {
+        Averager averager = new Averager();
+        averager.calculate("1:00:00", "1.0 GB","AMCHD");
+        averager.calculate("2:00:00", "1.0 GB","AMCHD");
+        assertThat(averager.retrieve("AMCHD"), equalTo((float) 0.75));
+    }
 
-    // Feed same channels and ask for average from same channel, return average
-    // Feed two channels, ask for one, give back average, pick other channel, give back that average
-    // Feed series in for multiple channels, ask for one that does not exist yet, average will be average of all.
+    @Test
+    public void canAverageAndRecallForMultipleChannels() {
+        Averager averager = new Averager();
+        averager.calculate("1:00:00", "1.0 GB", "AMCHD");
+        averager.calculate("2:00:00", "1.0 GB", "IFCHD");
+        assertThat(averager.retrieve("AMCHD"), equalTo((float) 1.0));
+        assertThat(averager.retrieve("IFCHD"), equalTo((float) 0.5));
+    }
 
+    @Test
+    public void canAverageMultipleChannelsAndReturnAverageOfAllForChannelsNotRecorded() {
+        Averager averager = new Averager();
+        averager.calculate("1:00:00","1.0 GB", "AMCHD");
+        averager.calculate("2:00:00","1.0 GB", "TWO");
+        averager.calculate("3:00:00","1.0 GB", "THREE");
+        assertThat(averager.retrieve("ANOTHER"), equalTo(((float) ((1.0+0.5+1.0/3.0)/3.0))));
+    }
 }
